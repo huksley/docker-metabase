065368b972e55e85a605b70708354e3c1c20e0bf
Ruslan Gainutdinov <huksley@wizecore.com>
2018-09-04 21:49:32 +0300

    Update build-artefacts.sh

--- build-artefacts.sh
+++ build-artefacts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 BIMG="huksley/metabase-build"
-METABASE_VERSION=${METABASE_VERSION:=v0.23.0}
+METABASE_VERSION=${METABASE_VERSION:=v0.30.1}
 
 set -e
 rm -Rf metabase
@@ -8,25 +8,10 @@ git clone https://github.com/metabase/metabase
 cd metabase
 git checkout tags/$METABASE_VERSION
 
-PULLS=${METABASE_PULLS:=4405}
+PULLS=${METABASE_PULLS:=7047}
 for N in `echo $PULLS`; do
 	echo "Applying pull request: $N"
     PURL="https://patch-diff.githubusercontent.com/raw/metabase/metabase/pull/$N.patch"
     curl -L -o pull-$N.patch $PURL
     patch -p1 < pull-$N.patch
 done
-
-PREV=`docker images $BIMG -q`
-if [ "$PREV" != "" ]; then
-	echo "Removing previous image $PREV"
-	docker rmi $PREV
-fi
-
-echo "Building compilation container..."
-docker build --tag $BIMG .
-
-echo "Getting files from container"
-CID=`docker create $BIMG`
-docker cp $CID:/app/source/bin/start ../start
-docker cp $CID:/app/source/target/uberjar/metabase.jar ../metabase.jar
-docker rm $CID
